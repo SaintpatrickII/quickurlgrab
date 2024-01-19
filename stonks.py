ad5e3130fd9843bb6ac659b945e3f9b08a1404c8
Patrick Govus <patrick@Patricks-MacBook-Pro.local>
2024-01-19 13:47:15 +0000

    added functionality for date portion of url

--- stonks.py
+++ stonks.py
@@ -1,12 +1,24 @@
 import requests
 import pandas as pd
 from io import BytesIO
+import time
 
 
 
+
+today = time.strftime("%Y-%m-%d")
+date_value = today.replace('-', '')
+print(date_value)
+
+
+# Working URL
 url_path = "https://www.ice.com/publicdocs/clear_europe/irmParameters/harmonized/ENERGY_MARGIN_SCANNING_20240115.CSV"
+
+url_for_reuse = f"https://www.ice.com/publicdocs/clear_europe/irmParameters/harmonized/ENERGY_MARGIN_SCANNING_{date_value}.CSV"
+
 resp = requests.get(url_path)
 by = resp.content
 df = pd.read_csv(BytesIO(by))
 print(df.head())
-df.to_csv("/Users/patrick/Desktop/stonks/willitwork.csv")
+date_value = 1
+df.to_csv(f"/Users/patrick/Desktop/stonks/{date_value}-report.csv")
